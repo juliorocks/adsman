bcb94bf2e0254543823a2846bb6bd8308c7b9d8a
juliorocks <30703281+juliorocks@users.noreply.github.com>
2026-02-17 09:48:06 -0300

    Expose env vars in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    META_APP_ID: process.env.META_APP_ID,
+    META_APP_SECRET: process.env.META_APP_SECRET,
+    META_REDIRECT_URI: process.env.META_REDIRECT_URI,
+  },
 };
 
 export default nextConfig;
